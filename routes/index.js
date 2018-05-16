d042c55f66e503b9117f0584b08a2b4565a6b981
zhuangrong2025 <42589963@qq.com>
2018-05-16 10:46:58 +0800

    server

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var child_process = require('child_process');
+
+//调用执行文件
+var openApp = function(){
+    child_process.execFile('2.bat',null,function (error,stdout,stderr) {
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+        else console.log('成功执行指令!');
+    });
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express1122-pm22' });
+  openApp();
+  console.log('正在执行bat文件--git pull');
+});
+
+
+module.exports = router;
